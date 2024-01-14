aeb5b86c73742f5225ff1501a7b8b8b258e496e2
ramandeepsingh <Ramandeep.singh6@cognizant.com>
2024-01-15 00:13:46 +0530

    first commit

---
+++ src/main/java/com/example/github/GithubApplication.java
@@ -0,0 +1,19 @@
+package com.example.github;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class GithubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubApplication.class, args);
+	}
+	@GetMapping("/hello")
+	public String helloWorld(){
+		return "Hello  World!!!";
+	}
+}
